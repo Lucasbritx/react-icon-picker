c143fb729b77458bfd4487187cf11fe9e65dd993
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-03-23 15:14:21 -0300

    deleted input component

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import typescript from "@rollup/plugin-typescript";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 export default {
-  input: "src/index.tsx",
+  IconPicker: "src/index.tsx",
   output: [
     {
       file: "dist/index.js",
